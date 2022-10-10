8a35e8d372b744b68b3dc7144963e02f231f776e
TimurShh <92332698+TimurShh@users.noreply.github.com>
2022-10-10 20:10:18 +0300

    Create 1330a.cpp

---
+++ shlanchak_t_v/1330a.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+ 
+int main() {
+    int t = 0;
+    std::cin >> t;
+    while (t--) {
+        int n, x = 0;
+        std::cin >> n >> x;
+        std::vector<int> a(n);
+        for (int i = 0; i < n; i++) {
+            int q = 0;
+            std::cin >> q;
+            a.push_back(q);
+        }
+        int ans = 0;
+        for (int i = 0;; i++) {
+            int f = 0;
+            for (int j = 0; j < a.size(); j++) {
+                if (a[j] == i) {
+                    f = 1;
+                }
+            }
+            if (f == 0) {
+                x -= 1;
+            }
+            if (x < 0) {
+                ans = i - 1;
+                break;
+            }
+        }
+ 
+        std::cout << ans << '\n';
+    }
+}
